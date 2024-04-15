41de5c408bc3c82937440250f2a9a24703c6c444
Pinaewskay Yana <pinaewskaya.yana@gmail.com>
2024-04-15 18:46:53 +0300

    Тестирование TextService

--- tests/textservice_test.py
+++ tests/textservice_test.py
@@ -1,6 +1,7 @@
 from  services.text_service import TextService
 from data.database_service import Databaseservice
 from config import settings
+from math import log
 import pytest
 
 
@@ -30,5 +31,42 @@ async def test_create_tf():
     assert created_tf_instance is not None
 
 
+@pytest.mark.asyncio
+async def test_count_doc():
+    data = [{"doc_id": "one", "word": "test1", "tf": 0.3},
+            {"doc_id": "two", "word": "test2", "tf": 0.55},
+            {"doc_id": "three", "word": "test3", "tf": 0.72},
+            {"doc_id": "four", "word": "test3", "tf": 0.19},
+            {"doc_id": "five", "word": "test5", "tf": 0.49}]
+    for note in data:
+        await text_service.create_tf(note["doc_id"], note["word"], note["tf"])
+    assert await text_service.count_doc() == len(data)
+
+
+
+@pytest.mark.asyncio
+async def test_count_doc_word():
+    assert await text_service.count_doc_word("test1") == 1
+
+
+
+@pytest.mark.asyncio
+async def test_get_idf():
+    count_doc = 5 # len(data) in test_count_doc
+    count_doc_word = 2 # word test3
+    delit = count_doc / count_doc_word
+    idf = log(delit)
+    assert abs(await text_service.get_idf("test3") - idf) < 0.00001
 
     
+@pytest.mark.asyncio
+async def test_create_idf():
+    created_idf_instance = await text_service.create_idf("test3")
+    assert created_idf_instance is not None
+
+
+@pytest.mark.asyncio
+async def test_check_idf():
+    assert await text_service.check_idf("test3") is not None
+    assert await text_service.check_idf("word") is False
+
